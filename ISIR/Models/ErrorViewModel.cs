e68fd6caba1e2dd680618963914ba05c6f513864
Peri <periyasamy@quantrium.ai>
2024-03-13 11:31:34 +0530

    Add project files.

---
+++ ISIR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISIR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
